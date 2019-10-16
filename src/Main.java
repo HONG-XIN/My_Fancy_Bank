32c40a59ca1e5fa0518c8c0744f525630dfc3279
dezhouw <hongsinwang@gmail.com>
2019-10-16 09:38:54 -0400

    add Name class and Date class

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import model.common.Date;
+
+/**
+ * This contains the main function to trigger the project.
+ */
+public class Main {
+    public static void main(String[] args) {
+        Date d = new Date(29, 2, 2000);
+
+    }
+}

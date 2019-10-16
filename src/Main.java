2da36871e9b50ef000cdbb922ff600e4404c9f34
dezhouw <hongsinwang@gmail.com>
2019-10-16 17:52:19 -0400

    add Password class

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,6 @@
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.println(new Email("helloworld@hotmail.com"));
+        System.out.println(new Password("Hellowo2rld@ho"));
     }
 }
